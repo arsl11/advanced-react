e8dc3950eb1b520011fa585388967b27b118f897
arsl11 <arifullin1100@mail.ru>
2023-07-10 01:27:25 +0300

    type compound component pattern

--- src/app/patterns/compound-component/page.tsx
+++ src/app/patterns/compound-component/page.tsx
@@ -1,55 +1,88 @@
-"use client"
+"use client";
 
-import React, { useCallback, useContext, useState } from "react";
+import React, { ReactNode, useCallback, useContext, useState } from "react";
 
-const MenuContext = React.createContext(false);
-
-const MenuAccordion = ({ children }) => {
-    const [activeGroup, setActiveGroup] = useState();
-
-    // stable link
-    const switchGroup = useCallback((title) => {
-        setActiveGroup((activeGroup) => activeGroup === title ? undefined : title)
-    }, [])
-
-    return <MenuContext.Provider value={{ activeGroup, switchGroup }}>{children}</MenuContext.Provider>
+interface MenuContextType {
+  activeGroup: string | undefined;
+  switchGroup: (title: string) => void;
 }
 
-MenuAccordion.Group = function MenuGroup({ children, title }) {
-    const { activeGroup, switchGroup } = useContext(MenuContext);
+interface MenuAccordionProps {
+  children?: ReactNode;
+}
 
-    return <div>
-        <button onClick={() => switchGroup(title)}>{title}</button>
-        {activeGroup === title && <div>{children}</div>}
+const MenuContext = React.createContext<MenuContextType | null>(null);
+
+const MenuAccordion = ({ children }: MenuAccordionProps) => {
+  const [activeGroup, setActiveGroup] = useState<string>();
+
+  // stable link
+  const switchGroup = useCallback((title: string) => {
+    setActiveGroup((activeGroup) =>
+      activeGroup === title ? undefined : title
+    );
+  }, []);
+
+  return (
+    <MenuContext.Provider value={{ activeGroup, switchGroup }}>
+      {children}
+    </MenuContext.Provider>
+  );
+};
+
+const useMenu = () => {
+  const menuContext = useContext(MenuContext);
+
+  if (!menuContext) {
+    throw new Error("useMenu has to be used within <MenuContext.Provider>");
+  }
+
+  return menuContext;
+};
+
+interface MenuGroupProps {
+  children?: React.ReactNode;
+  title: string;
+}
+
+MenuAccordion.Group = function MenuGroup({ children, title }: MenuGroupProps) {
+  const { activeGroup, switchGroup } = useMenu();
+
+  return (
+    <div>
+      <button onClick={() => switchGroup(title)}>{title}</button>
+      {activeGroup === title && <div>{children}</div>}
     </div>
+  );
+};
+
+interface MenuItemProps {
+  title: string;
 }
 
-MenuAccordion.Item = function MenuItem({ children, title }) {
-    return <div>{title}</div>
-}
+MenuAccordion.Item = function MenuItem({ title }: MenuItemProps) {
+  return <div>{title}</div>;
+};
 
 export default function CompoundComponent() {
-    return (
-        <MenuAccordion>
-            <MenuAccordion.Item title="Главная" />
-            <MenuAccordion.Group title="Фильмы">
-                <MenuAccordion.Item title="Топ" />
-                <MenuAccordion.Item title="Любимые" />
-                <MenuAccordion.Item title="Популярные" />
-            </MenuAccordion.Group>
-            <MenuAccordion.Group title="Сериалы">
-                <MenuAccordion.Item title="Топ" />
-                <MenuAccordion.Item title="Любимые" />
-                <MenuAccordion.Item title="Популярные" />
-            </MenuAccordion.Group>
-            <MenuAccordion.Group title="Служебное">
-                <MenuAccordion.Item title="О нас" />
-                <MenuAccordion.Item title="Вопросы" />
-                <MenuAccordion.Item title="Ответы" />
-            </MenuAccordion.Group>
-        </MenuAccordion>
-    );
+  return (
+    <MenuAccordion>
+      <MenuAccordion.Item title="Главная" />
+      <MenuAccordion.Group title="Фильмы">
+        <MenuAccordion.Item title="Топ" />
+        <MenuAccordion.Item title="Любимые" />
+        <MenuAccordion.Item title="Популярные" />
+      </MenuAccordion.Group>
+      <MenuAccordion.Group title="Сериалы">
+        <MenuAccordion.Item title="Топ" />
+        <MenuAccordion.Item title="Любимые" />
+        <MenuAccordion.Item title="Популярные" />
+      </MenuAccordion.Group>
+      <MenuAccordion.Group title="Служебное">
+        <MenuAccordion.Item title="О нас" />
+        <MenuAccordion.Item title="Вопросы" />
+        <MenuAccordion.Item title="Ответы" />
+      </MenuAccordion.Group>
+    </MenuAccordion>
+  );
 }
-
-
-
