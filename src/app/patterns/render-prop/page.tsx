7b308bb8bf55bb8b04c5b4893d66c141ac29cb3e
arsl11 <arifullin1100@mail.ru>
2023-07-10 01:59:06 +0300

    type render prop pattern

--- src/app/patterns/render-prop/page.tsx
+++ src/app/patterns/render-prop/page.tsx
@@ -1,14 +1,20 @@
 "use client";
 
-import { useState } from "react";
-
+import { ReactNode, useState } from "react";
+interface LayoutProps {
+  renderHeader?: () => ReactNode;
+  renderSidebarLeft?: (isOpen?: boolean) => ReactNode;
+  renderMainContent?: () => ReactNode;
+  renderSidebarRight?: () => ReactNode;
+  renderFooter?: () => ReactNode;
+}
 const Layout = ({
   renderHeader,
   renderFooter,
   renderMainContent,
   renderSidebarLeft,
   renderSidebarRight,
-}) => {
+}: LayoutProps) => {
   const [isOpen, setIsOpen] = useState<boolean>();
 
   return (
@@ -29,7 +35,9 @@ export default function RenderProp() {
   return (
     <Layout
       renderHeader={() => <header>Header</header>}
-      renderSidebarLeft={(isOpen) => <div>{isOpen ? "Open" : "Closed"}</div>}
+      renderSidebarLeft={(isOpen?: boolean) => (
+        <div>{isOpen ? "Open" : "Closed"}</div>
+      )}
       renderMainContent={() => <div>MainContent</div>}
       renderSidebarRight={() => <div>SidebarRight</div>}
       renderFooter={() => <footer>Footer</footer>}
